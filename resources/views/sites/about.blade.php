e7b2c624f03b3c10fe513ec5f4f41c826cc1c2b3
HardDrag <harddragoffical@gmail.com>
2020-02-06 10:19:43 +0100

    Layout for About page added

--- resources/views/sites/about.blade.php
+++ resources/views/sites/about.blade.php
@@ -3,6 +3,29 @@
 @section('title', 'About')   
 @section('content')
 
-<p>sample text</p>
+<body>
+    <div class="pt-3 table-responsive text-center">
+        <table class="table table-borderless" style="color: white">
+            <tbody>
+                <tr>
+                    <th>Our mission</th>
+                    <th>Our essence</th>
+                </tr>
+                <tr>
+                    <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi posuere convallis interdum. Ut pretium varius quam, non ultrices mi dignissim.</td>
+                    <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi posuere convallis interdum. Ut pretium varius quam, non ultrices mi dignissim.</td>
+                </tr>
+                <tr>
+                    <th>Our promise</th>
+                    <th>Our vibe</th>
+                </tr>
+                <tr>
+                    <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi posuere convallis interdum. Ut pretium varius quam, non ultrices mi dignissim.</td>
+                    <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi posuere convallis interdum. Ut pretium varius quam, non ultrices mi dignissim.</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</body>
 
 @endsection
