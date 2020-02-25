97993b7d5e098bd435b8d840940c514b27dcfae1
HardDrag <harddragoffical@gmail.com>
2020-02-25 20:31:46 +0100

    Added 24h delay check

--- resources/views/sites/show.blade.php
+++ resources/views/sites/show.blade.php
@@ -36,7 +36,15 @@
         <input type="hidden" name="_token" value="{{ csrf_token() }}">
     </form>
 
-    <a href="{{ route('site.edit',$site->id) }}" class="btn btn-outline-primary btn-lg">Edit</a>
+    @php
+        $db_date = $site -> created_at;
+        $db_date -> modify('+1 day');
+        $current_date = date('Y-m-d H:i:s');
+    @endphp
+    
+    @if ($db_date > $current_date) 
+        <a href="{{ route('site.edit',$site->id) }}" class="btn btn-outline-primary btn-lg">Edit</a>
+    @endif
 </div>
 
 @endsection
