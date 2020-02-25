8e75d57f6b55a3b26cd0686c8ad248eb35ad1b82
unknown <sawczukkarol9@gmail.com>
2020-02-25 14:35:50 +0100

    New About

--- resources/views/sites/about.blade.php
+++ resources/views/sites/about.blade.php
@@ -2,38 +2,30 @@
 
 @section('title', 'About')   
 @section('content')
+<!-- Styles -->
+<link href="{{ asset('css/about.css') }}" rel="stylesheet">
+<!-- Scripts -->
+<body class="text-center">
+    <div class="incontainer">
+        <img src="/images/1about.jpg" alt="hold" class="image i1" style="width:100%">
+        <div class="text middle m1">We just want to sip some good coffee with good people. If you want to check our goods before taking 12kg bag of beans,
+                come to our cafe and try it with us. Really, only we work in cafe.</div>
+        
+    </div>
 
-<body>
-    <div class="pt-3 table-responsive text-center">
-        <table class="table table-borderless" style="color: white">
-            <tbody>
-                <tr>
-                    <th>Our mission</th>
-                    <th>Our essence</th>
-                </tr>
-                <tr>
-                    <td>We just want to sip some good coffee with good people. If you want to check our goods before taking 12kg bag of beans, come to our cafe and try it with us. Really, only we work in cafe.
-                    <br/><br/><img src="/images/1about.jpg" alt="Lights" style="width:50%">
-                    </td>
+    <div class="incontainer">
+        <img src="/images/2about.jpg" alt="hold" class="image i2" style="width:100%">
+        <div class="text middle m2">Creating something more than just a drink was our dream and we are more than pleased to share it with you, the same goes with the beans.</div>
+    </div>
 
-                    <td>Creating something more than just a drink was our dream and we are more than pleased to share it with you, the same goes with the beans.
-                    <br/><br/><img src="/images/2about.jpg" alt="Lights" style="width:50%">
-                    </td>
-                </tr>
-                <tr>
-                    <th>Our promise</th>
-                    <th>Our vibe</th>
-                </tr>
-                <tr>
-                    <td>The only thing we can promise is that we will give our best to deliver the finest beans this planet ever seen.
-                    <br/><br/><img src="/images/3about.jpg" alt="Lights" style="width:50%">
-                    </td>
-                    <td>Best one.
-                    <br/><br/><img src="/images/4about.jpg" alt="Lights" style="width:50%">
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+    <div class="incontainer">
+        <img src="/images/3about.jpg" alt="hold" class="image i3" style="width:100%">
+        <div class="text middle m3">The only thing we can promise is that we will give our best to deliver the finest beans this planet ever seen.</div>
+    </div>
+
+    <div class="incontainer">
+        <img src="/images/4about.jpg" alt="hold" class="image i4" style="width:100%">
+        <div class="text middle m4">Best one.</div>
     </div>
 </body>
 
